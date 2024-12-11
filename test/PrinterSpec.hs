90c40d7bc619f7838ed6d8f961a9fede468f0edf
MartinFillon <martin.fillon@hotmail.com>
2024-12-11 10:37:06 +0100

    test: add unit test for the printer

--- test/PrinterSpec.hs
+++ test/PrinterSpec.hs
@@ -5,11 +5,29 @@
 -- PrinterSpec
 -}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module PrinterSpec (spec) where
 
-import Printer (Color (..), Paint (..), Style (..), reset)
-import Test.Hspec (Spec, context, describe, it, shouldBe)
+import Data.Either (rights)
+import Data.Text (Text)
+import Printer (
+    Color (..),
+    ColorCode (..),
+    Config (..),
+    Paint (..),
+    Style (..),
+    confDefaultValues,
+    confFilepath,
+    getColorCode,
+    parseColor',
+    parseColors,
+    parseConf,
+    parseRGB,
+    reset,
+ )
+import Test.Hspec (Spec, context, describe, it, shouldBe, shouldNotBe)
+import Test.Hspec.Megaparsec (shouldParse)
 
 newtype TestPaint = TestPaint Int
 
@@ -17,6 +35,9 @@ instance Paint TestPaint where
     paint :: Bool -> TestPaint -> String
     paint False (TestPaint i) = show i
     paint True (TestPaint i) = show Red ++ show i ++ reset
+
+testConf :: Text
+testConf = "warnings=\"255;0;255\"\nerrors=\"255;0;0\"\ninfos=\"0;0;255\""
 
 spec :: Spec
 spec = do
@@ -30,6 +51,11 @@ spec = do
             it "should be the green ansii code" $ do
                 let green = Green
                 show green `shouldBe` "\x1b[32m"
+
+        context "orange color" $ do
+            it "should be the orange ansii code" $ do
+                let orange = Orange
+                show orange `shouldBe` "\x1b[38;2;255;128;0m"
 
         context "blue color" $ do
             it "should be the blue ansii code" $ do
@@ -79,3 +105,99 @@ spec = do
             it "should return the number with red color" $ do
                 let testPaint = TestPaint 42
                 paint True testPaint `shouldBe` "\x1b[31m42\x1b[0m"
+
+        context "conf-file-path-const" $ do
+            it "should be the conf file path" $ do
+                confFilepath `shouldBe` "lisp-colors.conf"
+
+        context "conf-default-values-const" $ do
+            it "should be the default values" $ do
+                confDefaultValues
+                    `shouldBe` "warnings=\"255;0;255\"\nerrors=\"255;0;0\"\ninfos=\"0;0;255\""
+
+        context "show color" $ do
+            it "should show the color" $ do
+                show (RGB (ColorCode (255, 128, 0))) `shouldBe` "\x1b[38;2;255;128;0m"
+
+        context "get color code red" $ do
+            it "should show the color code" $ do
+                getColorCode Red `shouldBe` ColorCode (255, 0, 0)
+
+        context "get color code green" $ do
+            it "should show the color code" $ do
+                getColorCode Green `shouldBe` ColorCode (0, 255, 0)
+
+        context "get color code blue" $ do
+            it "should show the color code" $ do
+                getColorCode Blue `shouldBe` ColorCode (0, 0, 255)
+
+        context "get color code yellow" $ do
+            it "should show the color code" $ do
+                getColorCode Yellow `shouldBe` ColorCode (255, 255, 0)
+
+        context "get color code magenta" $ do
+            it "should show the color code" $ do
+                getColorCode Magenta `shouldBe` ColorCode (255, 0, 255)
+
+        context "get color code cyan" $ do
+            it "should show the color code" $ do
+                getColorCode Cyan `shouldBe` ColorCode (0, 255, 255)
+
+        context "get color code white" $ do
+            it "should show the color code" $ do
+                getColorCode White `shouldBe` ColorCode (255, 255, 255)
+
+        context "get color code orange" $ do
+            it "should show the color code" $ do
+                getColorCode Orange `shouldBe` ColorCode (255, 128, 0)
+
+        context "get RGB code" $ do
+            it "should show the color code" $ do
+                getColorCode (RGB (ColorCode (255, 128, 0))) `shouldBe` ColorCode (255, 128, 0)
+
+        context "parse RGB" $ do
+            it "parse RGB" $ do
+                parseRGB "127" "255" "233" `shouldBe` RGB (ColorCode (127, 255, 233))
+
+        context "parse conf" $ do
+            it "parse good conf" $ do
+                rights (parseConf testConf)
+                    `shouldBe` [ Config "warnings" "255;0;255",
+                                 Config "errors" "255;0;0",
+                                 Config "infos" "0;0;255"
+                               ]
+            it "parse bad conf" $ do
+                parseConf "caca" `shouldNotBe` []
+
+        context "parse colors from config" $ do
+            it "should work with basic colors" $ do
+                parseColors
+                    [ Config "warnings" "255;0;255",
+                      Config "errors" "255;0;0",
+                      Config "infos" "0;0;255"
+                    ]
+                    `shouldBe` Just
+                        ( RGB (ColorCode (255, 0, 255)),
+                          RGB (ColorCode (255, 0, 0)),
+                          RGB (ColorCode (0, 0, 255))
+                        )
+            it "should fail when missing colors" $ do
+                parseColors
+                    [ Config "warnings" "255;0;255",
+                      Config "errors" "255;0;0"
+                    ]
+                    `shouldBe` Nothing
+
+            it "should fail on bad color" $ do
+                parseColors
+                    [ Config "warnings" "255;0;255",
+                      Config "errors" "255;0;0",
+                      Config "infos" "0;a;255;"
+                    ]
+                    `shouldBe` Nothing
+
+            it "should parse color artinatively" $ do
+                parseColor' "255;0;255" `shouldBe` Just (RGB (ColorCode (255, 0, 255)))
+
+            it "should fail on bad color" $ do
+                parseColor' "255;0;255;" `shouldBe` Nothing
