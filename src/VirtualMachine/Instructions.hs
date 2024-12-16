bfb7121d10cde963cb363a2d160b496c2ae272b7
MartinFillon <martin.fillon@hotmail.com>
2024-12-16 23:11:50 +0100

    feat: add condition management

--- src/VirtualMachine/Instructions.hs
+++ src/VirtualMachine/Instructions.hs
@@ -5,32 +5,49 @@
 -- Instructions
 -}
 
-module VirtualMachine.Instructions (Insts (..), exec, execOp) where
+module VirtualMachine.Instructions (Insts (..), exec, execOp, Val (..)) where
 
 type Op = String
 
-data Insts
-    = Push Int
-    | Ret
-    | Call Op
+data Val
+    = N Int
+    | B Bool
     deriving (Show)
 
-type Stack = [Int]
+data Insts
+    = Push Val
+    | Ret
+    | Call Op
+    | JumpF Int
+    deriving (Show)
+
+type Stack = [Val]
 
 execOp :: String -> Stack -> Either String Stack
-execOp "Add" (x : y : stack) = Right ((+) x y : stack)
-execOp "Add" _ = Left "Add: Not enough args"
-execOp "Mul" (x : y : stack) = Right ((*) x y : stack)
-execOp "Mul" _ = Left "Mul: Not enough args"
-execOp "Sub" (x : y : stack) = Right ((-) x y : stack)
-execOp "Sub" _ = Left "Sub: Not enough args"
-execOp "Div" (_ : 0 : _) = Left "Div: Division by zero"
-execOp "Div" (x : y : stack) = Right (div x y : stack)
-execOp "Div" _ = Left "Div: Not enough args"
+execOp "Add" (N x : N y : stack) = Right (N ((+) x y) : stack)
+execOp "Add" _ = Left "Add: Bad args"
+execOp "Mul" (N x : N y : stack) = Right (N ((*) x y) : stack)
+execOp "Mul" _ = Left "Mul: Bad args"
+execOp "Sub" (N x : N y : stack) = Right (N ((-) x y) : stack)
+execOp "Sub" _ = Left "Sub: Bad args"
+execOp "Div" (_ : N 0 : _) = Left "Div: Division by zero"
+execOp "Div" (N x : N y : stack) = Right (N (div x y) : stack)
+execOp "Div" _ = Left "Div: Bad args"
+execOp "Eq" (N x : N y : stack) = Right (B ((==) x y) : stack)
+execOp "Eq" _ = Left "Eq: Bad args"
+execOp "Less" (N x : N y : stack) = Right (B ((<) x y) : stack)
+execOp "Less" _ = Left "Less: Bad args"
 execOp x _ = Left (x ++ ": Unknown")
 
-exec :: [Insts] -> Stack -> Either String Int
+skip :: [a] -> Int -> [a]
+skip [] _ = []
+skip x 0 = x
+skip (_ : xs) n = skip xs (n - 1)
+
+exec :: [Insts] -> Stack -> Either String Val
 exec (Ret : _) (x : _) = Right x
 exec (Push x : xs) s = exec xs (x : s)
 exec (Call x : xs) s = execOp x s >>= exec xs
+exec (JumpF n : xs) (B False : s) = exec (skip xs n) s
+exec (JumpF _ : xs) (B True : s) = exec xs s
 exec _ _ = Left "Missing infos"
