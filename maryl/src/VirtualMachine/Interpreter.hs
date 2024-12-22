23663bf0ca5089a9cb945259c8e06ccceda68fe4
Yanis <yanis.djeridi@epitech.eu>
2024-12-22 17:18:26 +0100

    feat(vm): add comparison and logical operators to interpreter

--- maryl/src/VirtualMachine/Interpreter.hs
+++ maryl/src/VirtualMachine/Interpreter.hs
@@ -129,3 +129,80 @@ operatorMod (N y : N x:_)
     | y == 0 = Left "modulo by zero"
     | otherwise = Right $ N (x `mod` y)
 operatorMod _ = Left "xpects two int"
+
+operatorEq :: [Value] -> Either String Value
+operatorEq (x : y : _) = Right $ B (x == y)
+operatorEq _ = Left "expects two value"
+
+operatorLt :: [Value] -> Either String Value
+operatorLt (y : x : _) =
+    case (x, y) of
+        (N a, N b) -> Right $ B (toDouble a < toDouble b)
+        (N a, D b) -> Right $ B (toDouble a < b)
+        (N a, F b) -> Right $ B (toDouble a < realToFrac b)
+        (D a, N b) -> Right $ B (a < toDouble b)
+        (D a, D b) -> Right $ B (a < b)
+        (D a, F b) -> Right $ B (a < realToFrac b)
+        (F a, N b) -> Right $ B (realToFrac a < toDouble b)
+        (F a, D b) -> Right $ B (realToFrac a < b)
+        (F a, F b) -> Right $ B (a < b)
+        _ -> Left "expects two number"
+operatorLt _ = Left "expects two number"
+
+operatorGt :: [Value] -> Either String Value
+operatorGt (y : x : _) =
+    case (x, y) of
+        (N a, N b) -> Right $ B (toDouble a > toDouble b)
+        (N a, D b) -> Right $ B (toDouble a > b)
+        (N a, F b) -> Right $ B (toDouble a > realToFrac b)
+        (D a, N b) -> Right $ B (a > toDouble b)
+        (D a, D b) -> Right $ B (a > b)
+        (D a, F b) -> Right $ B (a > realToFrac b)
+        (F a, N b) -> Right $ B (realToFrac a > toDouble b)
+        (F a, D b) -> Right $ B (realToFrac a > b)
+        (F a, F b) -> Right $ B (a > b)
+        _ -> Left "expects two number"
+operatorGt _ = Left "expects two number"
+
+operatorAnd :: [Value] -> Either String Value
+operatorAnd (B y : B x : _) = Right $ B (x && y)
+operatorAnd _ = Left "And expects two bool"
+
+operatorOr :: [Value] -> Either String Value
+operatorOr (B y : B x : _) = Right $ B (x || y)
+operatorOr _ = Left "Or expects two booleans"
+
+operatorPrint :: [Value] -> Either String Value
+operatorPrint (val : _) = Right $ N (fromIntegral (length (show val)))
+operatorPrint _ = Left "expects one val"
+
+operatorGet :: [Value] -> Either String Value
+operatorGet (N idx : L lst : _)
+    | idx >= 0 && idx < fromIntegral (length lst) = Right $ lst !! fromIntegral idx
+    | otherwise = Left "Index out of bound"
+operatorGet _ = Left "expects a list and an integer index"
+
+operatorSet :: [Value] -> Either String Value
+operatorSet (val : N idx : L lst : _)
+    | idx >= 0 && idx < fromIntegral (length lst) = Right $ L (take (fromIntegral idx) lst ++ [val] ++ drop (fromIntegral idx + 1) lst)
+    | otherwise = Left "Index out of bound"
+operatorSet _ = Left "expects a list, an integer index, and a value"
+
+initialMemory :: Memory
+initialMemory = Map.fromList
+    [
+        ("add", Op operatorAdd),
+        ("sub", Op operatorSub),
+        ("mul", Op operatorMul),
+        ("div", Op operatorDiv),
+        ("mod", Op operatorMod),
+        ("eq", Op operatorEq),
+        ("less", Op operatorLt),
+        ("greater", Op operatorGt),
+        ("and", Op operatorAnd),
+        ("or", Op operatorOr),
+        ("print", Op operatorPrint),
+        ("get", Op operatorGet),
+        ("set", Op operatorSet)
+    ]
+
