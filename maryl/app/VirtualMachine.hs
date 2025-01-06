988e546ce9042c4ef503541d65d52671edf6c23a
MartinFillon <martin.fillon@hotmail.com>
2025-01-06 08:47:33 +0100

    feat: add an error catcher to exit 84

--- maryl/app/VirtualMachine.hs
+++ maryl/app/VirtualMachine.hs
@@ -7,6 +7,7 @@
 
 module VirtualMachine (vm) where
 
+import Control.Exception (IOException, catch)
 import Control.Monad.State (evalStateT)
 import qualified Data.Map as Map
 import Utils (handleParseError, pError)
@@ -15,12 +16,16 @@ import VirtualMachine.Interpreter (exec, operators)
 import VirtualMachine.Parser (parseAssembly)
 import VirtualMachine.State (initialState)
 
+withExit :: IO () -> IO ()
+withExit f = f `catch` (\e -> pError (show (e :: IOException)))
+
 execParsed :: [Instruction] -> IO ()
 execParsed i =
-    evalStateT
-        exec
-        (initialState (jump Nothing (Right ".start") : i) (Map.fromList operators) [])
-        >>= print
+    withExit $
+        evalStateT
+            exec
+            (initialState (jump Nothing (Right ".start") : i) (Map.fromList operators) [])
+            >>= print
 
 vm :: Maybe String -> IO ()
 vm Nothing = pError "A file is required for the vm to run"
