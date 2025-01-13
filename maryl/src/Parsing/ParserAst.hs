05ba45758d13cfcab00c49fa7ce2e7d6a78a9639
Alexandre VIGOUREUX <alexandre.vigoureux@epitech.eu>
2025-01-13 20:26:32 +0100

    feat(compiler-parsing): structures declarations and structures assignations parsing

--- maryl/src/Parsing/ParserAst.hs
+++ maryl/src/Parsing/ParserAst.hs
@@ -22,6 +22,7 @@ module Parsing.ParserAst (
 
     -- ** Main parsing functions
     parseAST,
+    types,
     pAst,
     pTerm,
     pExpr,
@@ -38,6 +39,7 @@ module Parsing.ParserAst (
     pDeclarationVar,
     pFunc,
     pDeclarationFunc,
+    pDeclarationStruct,
     operatorTable,
     convertValue,
     getType,
@@ -96,6 +98,13 @@ data Variable = Variable
     }
     deriving (Eq, Ord, Show)
 
+-- | Structure structure containing the name of the structure, its type and its values in a block.
+data Structure = Structure
+    { sName :: String,
+      sProperties :: [Ast]
+    }
+    deriving (Eq, Ord, Show)
+
 -- | AST values parsed by the program.
 data Ast
     = AstVar String
@@ -115,6 +124,7 @@ data Ast
       AstTernary Ast Ast Ast
     | AstReturn Ast
     | AstBlock [Ast]
+    | AstStruct [Ast]
     | -- | condition (AstBlock to loop in)
       AstLoop Ast Ast
     | -- | break statement
@@ -125,11 +135,10 @@ data Ast
     | AstDefineFunc Function
     | -- | nameLoop condition do
       AstDefineLoop String Ast Ast
+    | AstDefineStruct Structure
     | AstList [Ast]
     | -- | variable indexes
       AstListElem String [Int]
-    | -- | name AstBlock
-      AstStruct String Ast
     deriving (Eq, Ord)
 
 instance Show Ast where
@@ -157,6 +166,8 @@ instance Show Ast where
     show (AstDefineLoop nLoop cond loopBlock) = "defLoop " ++ show nLoop ++ "(" ++ show cond ++ "){" ++ show loopBlock ++ "}"
     show (AstList asts) = "[]" ++ show asts
     show (AstListElem var idxs) = show var ++ "[" ++ intercalate "][" (map show idxs) ++ "]"
+    show (AstStruct s) = "struct " ++ show s
+    show (AstDefineStruct s) = "struct " ++ sName s ++ " " ++ show (sProperties s)
 
 -- | Types handled by the program.
 data MarylType = String | Int | Double | Char | Bool | Void | List MarylType | Const MarylType | Struct String | Undefined
@@ -201,6 +212,7 @@ rWords =
              "return",
              "null",
              "const",
+             "struct",
              "break",
              "continue"
            ]
@@ -281,6 +293,7 @@ convertValue =
           AstBool <$> bool,
           AstChar <$> charLiteral,
           AstString <$> stringLiteral,
+          AstStruct <$> try struct,
           AstBlock <$> block,
           try pFunc,
           AstVoid <$ try (lexeme $ string "null"),
@@ -310,6 +323,9 @@ listVariables' =
 block :: Parser [Ast]
 block = between (symbol "{") (symbol "}") (many pTerm)
 
+struct :: Parser [Ast]
+struct = between (symbol "{") (symbol "}") (convertValue `sepBy` lexeme ",")
+
 types' :: [String]
 types' =
     [ "int",
@@ -320,8 +336,23 @@ types' =
       "void"
     ]
 
+pStruct :: Parser String
+pStruct = lexeme $ do
+    s <- string "struct"
+    sc
+    n <- variable
+    return $ s ++ " " ++ n
+
 types :: Parser String
-types = choice (map string (("[]" ++) <$> types')) <|> choice (map string types')
+types = choice
+    [ choice (map string $ ("[]" ++) <$> types')
+    , choice (map string $ ("const " ++) <$> types')
+    , choice (map string types')
+    , pStruct
+    ]
+
+trimFront :: String -> String -> String
+trimFront toTrim str = dropWhile (\x -> x == ' ' || x == '\t') (fromJust $ stripPrefix toTrim str)
 
 -- | Returns a 'MarylType' based on string given as parameter. If the string is not supported, returns 'Undefined'.
 getType :: String -> MarylType
@@ -333,7 +364,8 @@ getType "bool" = Bool
 getType "void" = Void
 getType str
     | "[]" `isPrefixOf` str = List $ getType (fromJust $ stripPrefix "[]" str)
-    | "const" `isPrefixOf` str = Const $ getType (dropWhile (\x -> x == ' ' || x == '\t') (fromJust $ stripPrefix "const" str))
+    | "const" `isPrefixOf` str = Const $ getType $ trimFront "const" str
+    | "struct" `isPrefixOf` str = Struct $ takeWhile (\x -> x /= ' ' && x /= '\t') $ trimFront "struct" str
     | otherwise = Undefined
 
 optionalValue :: Parser (Maybe Ast)
@@ -372,6 +404,18 @@ pDeclarationFunc = do
     b <- block
     return $
         AstDefineFunc (Function {fName = n, fArgs = a, fBody = b, fType = getType t})
+
+{- | Parsing 'pStruct' declaration, formatted: struct name {}
+
+>>> struct vector {int x; int y;}
+-}
+pDeclarationStruct :: Parser Ast
+pDeclarationStruct = do
+    symbol "struct" >> sc
+    n <- variable
+    sc
+    b <- block
+    return $ AstDefineStruct (Structure {sName = n, sProperties = b})
 
 {- | Function names must be formatted like a 'variable', followed by parenthesis: foo()
 
@@ -534,6 +578,7 @@ pTerm =
           pIf,
           pLoop,
           try pDeclarationFunc,
+          try pDeclarationStruct,
           pDeclarationVar <* semi,
           pBreak <* semi,
           pContinue <* semi,
